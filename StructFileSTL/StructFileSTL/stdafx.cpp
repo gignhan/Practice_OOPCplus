4158333d3f969ec2a95a3315af021235cfb34b51
XuanNhan <xuannhan2905@gmail.com>
2019-11-21 11:40:03 +0700

    Create project StructFileSTL

---
+++ StructFileSTL/StructFileSTL/stdafx.cpp
@@ -0,0 +1,10 @@
+// stdafx.cpp : source file that includes just the standard includes
+// StructFileSTL.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
