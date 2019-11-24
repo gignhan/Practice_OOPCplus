49d337c2f37c7cfa69bb9b0957432cf973893914
XuanNhan <xuannhan2905@gmail.com>
2019-11-24 11:44:13 +0700

    create function DoStart of class Patient

--- VirusMain/VirusMain/Patient.cpp
+++ VirusMain/VirusMain/Patient.cpp
@@ -4,9 +4,43 @@
 
 Patient::Patient()
 {
+	m_resistance = 1000 + rand()%8001;
 }
 
 
 Patient::~Patient()
 {
+}
+void Patient::DoStart()
+{
+	int x = 10 + rand() % 11;
+	for (int i = 0; i < x; i++)
+	{
+		int k = rand() % 2;
+		if (k == 1) {
+			Virus *v = new FluVirus();
+			v->DoBorn();
+			m_viruslist.push_back(v);
+		}
+		else
+		{
+			Virus *v = new DengueVirus();
+			v->DoBorn();
+			m_viruslist.push_back(v);
+		}
+		
+	}
+	m_state = 1;
+}
+void Patient::TakeMedicine()
+{
+
+}
+void Patient::DoDie()
+{
+
+}
+void Patient::GetState()
+{
+
 }
--- VirusMain/VirusMain/Patient.h
+++ VirusMain/VirusMain/Patient.h
@@ -1,11 +1,14 @@
 #pragma once
 #include "Virus.h"
+#include"DengueVirus.h"
+#include"FluVirus.h"
 class Patient
 {
 protected: int m_resistance, m_state; list<Virus*> m_viruslist;
 public:
 	Patient();
 	~Patient();
+
 	void DoStart();
 	void TakeMedicine();
 	void DoDie();
